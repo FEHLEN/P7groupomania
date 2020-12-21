9114ff80e6cb9d882139774d2bc6b3d4318ea58b
Fehlen <fehlenlili64100@gmail.com>
2020-12-21 19:36:17 +0100

    ajout de fichiers

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+//Routes des CRUD
+const routesPosts = require('./routes/routesPosts');
+const routesUsers = require('./routes/routesUsers');
+const routesMod = require('./routes/routesMod');
+const routesGifs = require('./routes/routesGifs');
+
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+//Endpoints utilisés par l'API du serveur localhost: 3000
+app.use('/api/posts', routesPosts);
+app.use('/api/auth', routesUsers);
+app.use('/api/moderation', routesMod);
+app.use('/api/gifs', routesGifs);
+
+module.exports = app;
